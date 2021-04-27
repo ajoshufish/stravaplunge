54d1c3f2ba7e54dd38c6e304082e485ea2c1ef73
Joshu Fisher <joshu.fisher@gmail.com>
2021-04-27 19:38:41 -0400

    Rip out plotly express and add standard plotly

--- stravacode.py
+++ stravacode.py
@@ -1,10 +1,9 @@
 import pandas as pd 
 import streamlit as st 
-import os
 from datetime import datetime
-import plotly.express as px
-
-#pd.options.plotting.backend = "plotly"
+import os
+import plotly.graph_objects as go 
+from plotly.subplots import make_subplots
 
 st.title('Strava Data Analysis -- one user: me!')
 
@@ -24,7 +23,7 @@ df['Activity_Date'] = pd.to_datetime(df['Activity_Date'])
 actType = st.sidebar.radio('Activity Type',['Run','Bike'])
 startDay = st.sidebar.slider('Start Date', value=datetime(2020, 7, 30),format="MM/DD/YY", min_value=datetime(2020,3,1), max_value=datetime(2021,2,2))
 endDay = st.sidebar.slider('End Date', value=datetime(2020, 10, 30),format="MM/DD/YY", min_value=datetime(2020,3,1), max_value=datetime(2021,2,3))
-#unitChoice = st.sidebar.radio('Units', ['Imperial','Metric'])
+#unitChoice = st.sidebar.radio('Units', ['Imperial','Metric'])  #strava backend is metric so at some point we'll need to convert stuff
 
 #Apply our options
 filt = df[(df.Activity_Date >= startDay) & (df.Activity_Date <= endDay) & (df.Activity_Type == actType)]
@@ -35,11 +34,17 @@ optDict = {'Avg. HR':'Average_Heart_Rate', 'Avg. Pace':'Average_Speed', 'Dist':'
 chartOptions = st.multiselect('What are we looking at?', ['Avg. HR', 'Avg. Pace', 'Dist', 'Vert', 'Rel. Effort'], default =['Dist','Avg. Pace'])
 
 #convert them to df column labels
-chartCats = []
+chartCats = ['Activity_Date']
 for i in chartOptions:
     chartCats.append(optDict[i])
 
-fig = px.line(filt[chartCats])
+fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+fig.add_trace(go.Scatter(name="Distance", y=filt['Distance'], x=filt['Activity_Date']), secondary_y=False)
+
+fig.add_trace(go.Scatter(name="Vert", y=filt['Elevation_Gain'], x=filt['Activity_Date']), secondary_y=True)
+
+
 st.plotly_chart(fig)
 #st.line_chart(filt[chartCats])
 #st.dataframe(df.dtypes)
