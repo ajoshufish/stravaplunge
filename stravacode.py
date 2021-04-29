5d2b7f9ee748bb41d141ade8816c5fad5bb6121a
Joshu Fisher <joshu.fisher@gmail.com>
2021-04-29 10:50:00 -0400

    revert date range focus, prevent empty df error in charting

--- stravacode.py
+++ stravacode.py
@@ -19,13 +19,18 @@ df = load_dataset()
 
 #tidy up column info
 df.columns = df.columns.str.replace(' ', '_')
-df = df.sort_values(by='Activity_Date')
+#df = df.sort_values(by='Activity_Date')
 df['Activity_Date'] = pd.to_datetime(df['Activity_Date'])
 df['Average_Speed'] = df['Distance.1']/df['Moving_Time']
 
 #Option Selection
-startDay = st.sidebar.slider('Start Date', value=datetime(2020, 7, 30),format="MM/DD/YY", min_value=df.head(1)['Activity_Date'].iloc[0].to_pydatetime(), max_value=df.tail(1)['Activity_Date'].iloc[0].to_pydatetime())
-endDay = st.sidebar.slider('End Date', value=datetime(2020, 10, 30),format="MM/DD/YY", min_value=df.head(1)['Activity_Date'].iloc[0].to_pydatetime(), max_value=df.tail(1)['Activity_Date'].iloc[0].to_pydatetime() )
+startDay = st.sidebar.slider('Start Date', value=datetime(2020, 2, 1),format="MM/DD/YY", min_value=datetime(2020, 2, 1), max_value=datetime(2021, 2, 1))
+endDay = st.sidebar.slider('End Date', value=datetime(2021, 1, 30),format="MM/DD/YY", min_value=datetime(2020, 2, 2), max_value=datetime(2021, 2, 1) )
+
+#We can switch to these to give us the full range of activities
+#startDay = st.sidebar.slider('Start Date', value=datetime(2020, 2, 1),format="MM/DD/YY", min_value=df.head(1)['Activity_Date'].iloc[0].to_pydatetime(), max_value=df.tail(1)['Activity_Date'].iloc[0].to_pydatetime())
+#endDay = st.sidebar.slider('End Date', value=datetime(2021, 1, 30),format="MM/DD/YY", min_value=df.head(1)['Activity_Date'].iloc[0].to_pydatetime(), max_value=df.tail(1)['Activity_Date'].iloc[0].to_pydatetime() )
+
 #unitChoice = st.sidebar.radio('Units', ['Imperial','Metric'])  #strava backend is metric so at some point we'll need to convert stuff
 
 #Apply our options
@@ -42,27 +47,33 @@ chartCats = ['Activity_Date']
 for i in chartOptions:
     chartCats.append(optDict[i])
 
-fig = make_subplots(specs=[[{"secondary_y": True}]])
+
 
 #build out individual lines based on selected options
 #amazing tds post for the px and go mixing to get the nonlinear trendlines I wanted:
 #https://towardsdatascience.com/time-series-and-logistic-regression-with-plotly-and-pandas-8b368e76b19f
+def makeChart():
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+    for i in enumerate(chartOptions):
+        if i[1] == 'Vert':
+            fig.add_trace(go.Scatter(name=i[1], y=filt[optDict[i[1]]], x=filt['Activity_Date']), secondary_y=False)
+        else:
+            fig.add_trace(go.Scatter(name=i[1], y=filt[optDict[i[1]]], x=filt['Activity_Date']), secondary_y=True)
+        trend_fig = px.scatter(filt, x=filt['Activity_Date'], y=filt[optDict[i[1]]], trendline="lowess")
+        x_trend = trend_fig["data"][1]['x']
+        y_trend = trend_fig["data"][1]['y']
+        fig.add_trace(go.Scatter(x=x_trend, y=y_trend, name=i[1]+' trend', line = dict(width=4, dash='dash')))
 
-for i in enumerate(chartOptions):
-    if i[1] == 'Vert':
-        fig.add_trace(go.Scatter(name=i[1], y=filt[optDict[i[1]]], x=filt['Activity_Date']), secondary_y=False)
-    else:
-        fig.add_trace(go.Scatter(name=i[1], y=filt[optDict[i[1]]], x=filt['Activity_Date']), secondary_y=True)
-    trend_fig = px.scatter(filt, x=filt['Activity_Date'], y=filt[optDict[i[1]]], trendline="lowess")
-    x_trend = trend_fig["data"][1]['x']
-    y_trend = trend_fig["data"][1]['y']
-    fig.add_trace(go.Scatter(x=x_trend, y=y_trend, name=i[1]+' trend', line = dict(width=4, dash='dash')))
-
-#tidy up some display options and display the compiled chart
-colorhelp = 'rgba(0,0,0,0)'
-fig.update_layout(margin_l=10, margin_r=10, margin_t=10, margin_b=10, hovermode='x', showlegend=True, paper_bgcolor=colorhelp, plot_bgcolor=colorhelp)
-st.text("Let's throw all those on a simple chart, shall we?")
-st.plotly_chart(fig , use_container_width=True)
+    #tidy up some display options and display the compiled chart
+    colorhelp = 'rgba(0,0,0,0)'
+    fig.update_layout(margin_l=10, margin_r=10, margin_t=10, margin_b=10, hovermode='x', showlegend=True, paper_bgcolor=colorhelp, plot_bgcolor=colorhelp)
+    return fig
+if filt.empty:
+    st.text('Widen the date range to select some activities!')
+else:
+    chart = makeChart()
+    st.text("Let's throw all those on a simple chart, shall we?")
+    st.plotly_chart(chart , use_container_width=True)
 
 #st.line_chart(filt[chartCats[1:]])
 #st.dataframe(df.dtypes)
