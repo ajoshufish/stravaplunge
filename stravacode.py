9f411ae26645e1493ba6ae7725680d376059dafc
Joshu Fisher <joshu.fisher@gmail.com>
2021-04-29 13:58:41 -0400

    switch to gspread and streamlit secrets

--- stravacode.py
+++ stravacode.py
@@ -5,23 +5,53 @@ import os
 import plotly.graph_objects as go 
 from plotly.subplots import make_subplots
 import plotly_express as px
+import gspread
 
 st.set_page_config(layout='wide')
 st.title('Strava Data Analysis -- one user: me!')
 
+#setup data load config based on streamlit secrets
+credentials = {
+  "type": st.secrets["type"],
+  "project_id": st.secrets["project_id"],
+  "private_key_id": st.secrets["private_key_id"],
+  "private_key": st.secrets["private_key"],
+  "client_email": st.secrets["client_email"],
+  "client_id": st.secrets["client_id"],
+  "auth_uri": st.secrets["auth_uri"],
+  "token_uri": st.secrets["token_uri"],
+  "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
+  "client_x509_cert_url": st.secrets["client_x509_cert_url"]
+  }
+sheetKey = st.secrets["worksheet_key"]
+dbSheet = st.secrets["sheet"]
+
 #Load up all our data
 @st.cache(allow_output_mutation=True)
 def load_dataset():
-    filefold = os.path.dirname(os.path.abspath(__file__))
-    datapath = os.path.join(filefold, 'activities.csv')
-    return pd.read_csv(datapath)
-df = load_dataset()
+   gc = gspread.service_account_from_dict(credentials)
+   ws = gc.open_by_key(sheetKey).worksheet(dbSheet)
+   return ws.get_all_records()
+
+data = load_dataset()
+headers = data.pop(0)
+df = pd.DataFrame(data, columns=headers)
 
 #tidy up column info
 df.columns = df.columns.str.replace(' ', '_')
-#df = df.sort_values(by='Activity_Date')
+df = df.convert_dtypes()
 df['Activity_Date'] = pd.to_datetime(df['Activity_Date'])
-df['Average_Speed'] = df['Distance.1']/df['Moving_Time']
+df = df.sort_values(by='Activity_Date')
+df['Distance'] = pd.to_numeric(df['Distance'])
+df['Elapsed_Time'] = pd.to_numeric(df['Elapsed_Time'])
+df['Elevation_Gain'] = pd.to_numeric(df['Elevation_Gain'])
+df['Relative_Effort'] = pd.to_numeric(df['Relative_Effort'])
+df['Average_Heart_Rate'] = pd.to_numeric(df['Average_Heart_Rate'])
+df['Average_Cadence'] = pd.to_numeric(df['Average_Cadence'])
+df['Average_Speed'] = df['Distance']/df['Elapsed_Time']
+
+st.write(df.dtypes)
+
 
 #Option Selection
 startDay = st.sidebar.slider('Start Date', value=datetime(2020, 2, 1),format="MM/DD/YY", min_value=datetime(2020, 2, 1), max_value=datetime(2021, 2, 1))
@@ -39,15 +69,13 @@ filt.sort_values(by='Activity_Date')
 st.dataframe(filt)
 
 #Collect together options for our line graph
-optDict = {'Avg. HR':'Average_Heart_Rate', 'Avg. Pace':'Average_Speed', 'Dist':'Distance', 'Vert':'Elevation_Gain', 'Rel. Effort':'Relative_Effort'}
-chartOptions = st.multiselect('What are we looking at?', ['Avg. HR', 'Avg. Pace', 'Dist', 'Vert', 'Rel. Effort'], default =['Dist','Avg. Pace'])
+optDict = {'Avg. Cadence':'Average_Cadence','Avg. HR':'Average_Heart_Rate', 'Avg. Pace':'Average_Speed', 'Dist':'Distance', 'Vert':'Elevation_Gain', 'Rel. Effort':'Relative_Effort'}
+chartOptions = st.multiselect('What are we looking at?', ['Avg. HR', 'Avg. Pace', 'Dist', 'Vert', 'Rel. Effort', 'Avg. Cadence'], default =['Dist','Avg. Pace'])
 
 #convert them to df column labels
 chartCats = ['Activity_Date']
 for i in chartOptions:
     chartCats.append(optDict[i])
-
-
 
 #build out individual lines based on selected options
 #amazing tds post for the px and go mixing to get the nonlinear trendlines I wanted:
@@ -68,12 +96,16 @@ def makeChart():
     colorhelp = 'rgba(0,0,0,0)'
     fig.update_layout(margin_l=10, margin_r=10, margin_t=10, margin_b=10, hovermode='x', showlegend=True, paper_bgcolor=colorhelp, plot_bgcolor=colorhelp)
     return fig
+
+#Let's show the chart....assuming we chose options properly
 if filt.empty:
     st.text('Widen the date range to select some activities!')
+elif len(chartOptions) == 0:
+    st.text('Choose some options to explore!')
 else:
     chart = makeChart()
     st.text("Let's throw all those on a simple chart, shall we?")
     st.plotly_chart(chart , use_container_width=True)
 
-#st.line_chart(filt[chartCats[1:]])
-#st.dataframe(df.dtypes)
+
+
